3ebacd5289f65953f4242de77d46df03cf832c86
fbottazzi <francobottazzi2014@gmail.com>
2024-06-09 15:31:48 -0300

    Cambio de nombres

--- TP12/main.c
+++ TP12/main.c
@@ -1,60 +1,40 @@
-// Modulo de manejo de gpio_out
-#include <stdlib.h>
-#include <stdio.h>
 #include "constants.h"
-#include <stdint.h>
+#include "gpio_manage.h"
+#include <stdio.h>
 
-typedef uint8_t pin_t;
-
-// Variables globales
-
-
-static const char export_path[] = "/sys/class/gpio/export";
-static const char gpio_dir_path[] = "/sys/class/gpio/gpio";
-static char string_buffer[ sizeof(gpio_dir_path) + 20 ]; // esto para tener espacio para copiar los strings
-
-static FILE* gpio_pins[NUM_OF_PINS]; // como arrancan en NULL, el programa puede ver si estan inicializados o no
-static FILE* export_handle = NULL;
-
-// funciones
-
-int init_gpio_out_module();
-
-int init_gpio_out_pin(pin_t pin);
-
-int main (void){
-	return 0;
-}
-
-
-int init_gpio_out_module() {
-	static int first_time = 1;
-
-	if(first_time) {
-		
-		export_handle = fopen(export_path, "w");
-
-		if(export_handle == NULL) return ERR_UNACCESIBLE;
-		
-		first_time = 0;
+int main()
+{
 	
+	pin_t led = {"23"};
+
+	if(init_gpio_out_module() < 0) {
+		printf("ERROR DE INIT MODULE\n");
+		return 0;
 	}
-	return 0;
-}
+	int result =  init_gpio_out_pin(1, &led);
+	switch(result) {
+	case GPIO_UNINITIATED:
+		printf("ERROR DE INIT DE MODULO\n");
+		break;
+	case ERR_CANT_EXPORT:
+		printf("ERROR NO PUDO EXPORTAR\n");
+		break;
+	case ERR_UNACCESIBLE:
+		printf("ERROR NO PUDO ABRIR VALUE\n");
+		break;
+	case ERR_DIRECTION:
+		printf("ERROR NO PUDO ABRIR ARCHIVO DIRECTION\n");
+		break;
+	}
 
-int init_gpio_out_pin(pin_t pin) {
-	
-	// Valido q pueda arrancar
-	if(pin > NUM_OF_PINS || pin < 0) return INVALID_INPUT;
-	if(export_handle == NULL) return GPIO_UNINITIATED;
-	
-	/*int result;
-	result = fputs( itoa(pin, string_buffer, 10), export_handle); // pone "17", "23" o lo q sea en el export.
-	if(result == -1) return ERR_UNACCESIBLE;
-	*/
-	//comentado pq itoa aparentemente no esta en stdlib xd
+	if (result < 0) return 1;
 
-	// Habria q hacer un strcpy del gpio_dir_path al buffer, dsps sprintf("%s%d/value") de  itoa del pin al buffer, despues value
+	int value = 0;
+	while(1) {
+		while(getchar() != '\n');
+		set_pin(led, value);
+		value = !value;
+	}
 
 	return 0;
 }
